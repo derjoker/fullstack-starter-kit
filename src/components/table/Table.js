fb11aca457a7b4cd539a31ca02c02c4aede63d5d
Feng Ya <fengya04@126.com>
2017-08-13 17:03:46 +0800

    save

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,14 +1,36 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import ReactTable from 'react-table'
+import { omit } from 'lodash'
 
-const Table = ({ loading, data = [] }) => {
+const Table = ({ loading, data = [], save }) => {
+  const renderEditable = cellInfo => {
+    return (
+      <div
+        style={{ backgroundColor: '#fafafa' }}
+        contentEditable
+        suppressContentEditableWarning
+        onBlur={e => {
+          const prev = omit(data[cellInfo.index], '__typename')
+          const record = {...prev}
+          record[cellInfo.column.id] = e.target.innerText
+          console.table([prev, record])
+          save && save(record)
+        }}
+        dangerouslySetInnerHTML={{
+          __html: data[cellInfo.index][cellInfo.column.id]
+        }}
+      />
+    )
+  }
+
   const columns = [
     {Header: 'Name', accessor: 'name', minWidth: 50},
     {Header: 'Email', accessor: 'email'},
-    {Header: 'Address', accessor: 'address'},
-    {Header: 'Age', accessor: 'age'}
+    {Header: 'Address', accessor: 'address', Cell: renderEditable},
+    {Header: 'Age', accessor: 'age', Cell: renderEditable}
   ]
+
   return (
     <div>
       <ReactTable
@@ -24,6 +46,7 @@ const Table = ({ loading, data = [] }) => {
 }
 
 Table.propTypes = {
+  loading: PropTypes.bool,
   data: PropTypes.array,
   save: PropTypes.func
 }
--- src/containers/User.js
+++ src/containers/User.js
@@ -1,27 +1,52 @@
 import React from 'react'
-import { gql, graphql } from 'react-apollo'
+import { gql, graphql, compose } from 'react-apollo'
 
 import Table from '../components/table/Table'
 
-const User = ({ data }) => {
+const User = ({ data, mutate }) => {
   const { loading, users } = data
   return (
     <div>
-      <Table loading={loading} data={users} />
+      <Table
+        loading={loading}
+        data={users}
+        save={record => mutate({
+          variables: {user: record}
+        })} />
     </div>
   )
 }
 
-const query = gql`
-  query {
-    users: findUsers (condtion: {}) {
-      id
-      name
-      email
-      address
-      age
-    }
+const USER_QUERY = gql`
+query {
+  users: findUsers (condtion: {}) {
+    id
+    name
+    email
+    address
+    age
   }
+}
+`
+const withUsers = graphql(USER_QUERY)
+
+const USER_UPDATE = gql`
+mutation updateUser ($user: UserInput!) {
+  user: updateUser (user: $user) {
+    id
+    name
+    email
+    address
+    age
+  }
+}
 `
 
-export default graphql(query)(User)
+const withUpdateUser = graphql(USER_UPDATE)
+
+const withData = compose(
+  withUsers,
+  withUpdateUser
+)
+
+export default withData(User)
