198ab1795c1bbee42c20f58d7fb3e3a8147cda7c
Feng Ya <fengya04@126.com>
2017-08-15 19:07:08 +0800

    fix user

--- src/containers/User.js
+++ src/containers/User.js
@@ -3,12 +3,20 @@ import { gql, graphql, compose } from 'react-apollo'
 
 import Table from '../components/table/Table'
 
+const columns = [
+  {Header: 'Name', accessor: 'name', minWidth: 50},
+  {Header: 'Email', accessor: 'email'},
+  {Header: 'Address', accessor: 'address', editable: true},
+  {Header: 'Age', accessor: 'age', editable: true}
+]
+
 const User = ({ data, mutate }) => {
   const { loading, users } = data
   return (
     <div>
       <Table
         loading={loading}
+        columns={columns}
         data={users}
         save={record => mutate({
           variables: {user: record}
